d6ee8722d0a19ec2a88516d6623c88c4ad5963c8
Maksim Kostyuk <kostyuk.job@gmail.com>
2025-06-05 21:38:00 +0600

    feat: to CommonChecks class added custom assertions with result logging, check methods in CommonChecks and PetChecks refactored with these new methods

--- src/test/java/cheks/PetChecks.java
+++ src/test/java/cheks/PetChecks.java
@@ -4,10 +4,6 @@ import io.qameta.allure.Allure;
 import io.qameta.allure.Step;
 import io.restassured.response.Response;
 import models.pet.Pet;
-import org.junit.jupiter.api.Assertions;
-
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-import static requests.BaseRequest.fromJson;
 
 public class PetChecks {
 
@@ -15,7 +11,7 @@ public class PetChecks {
     public static void checkPetBody(Pet expectedPet, Response response) {
         Allure.addAttachment("Expected body", expectedPet.toString());
         Allure.addAttachment("Actual body", response.body().asString());
-        Pet actualPet = assertDoesNotThrow(() -> fromJson(response.asString(), Pet.class));
-        Assertions.assertEquals(expectedPet, actualPet, "Received pet body does not equal to expected one");
+        Pet actualPet = CommonChecks.assertionParseSuccessful(response.asString(), Pet.class);
+        CommonChecks.assertionEquals(expectedPet, actualPet, "Received pet body does not equal to expected one");
     }
 }

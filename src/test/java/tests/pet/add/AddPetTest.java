b35fd0d8d212106e13eb95a12a06268e1fd366bb
Maksim Kostyuk <kostyuk.job@gmail.com>
2025-06-05 23:19:20 +0600

    ref: AddPetTest now extends BaseTestClass; positive test not parameterized now; deleting of created pet moved to AfterEach method; added logging of test start and end

--- src/test/java/tests/pet/add/AddPetTest.java
+++ src/test/java/tests/pet/add/AddPetTest.java
@@ -4,24 +4,43 @@ import cheks.CommonChecks;
 import cheks.PetChecks;
 import io.qameta.allure.Description;
 import io.restassured.response.Response;
+import lombok.extern.slf4j.Slf4j;
 import models.pet.Pet;
 import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.Test;
 import requests.PetRequest;
+import tests.BaseTestClass;
 
+import static data.provider.pet.PetProvider.singleValidPetProvider;
+
+@Slf4j
 @DisplayName("Add pet api tests")
-public class AddPetTest {
+public class AddPetTest extends BaseTestClass {
 
-    @ParameterizedTest
-    @MethodSource("data.provider.pet.PetProvider#singleValidPetProvider")
-    @DisplayName("Positive test")
+    private static Pet pet;
+
+    @BeforeAll
+    public static void setUp() {
+        pet = singleValidPetProvider().findAny().get();
+    }
+
+    @Test
+    @DisplayName("Positive test to add pet")
     @Description("Sending valid pet body to api point. Should receive code 200 and body with the same pet parameters")
-    public void positiveTestShouldReturn200AndBodyEqualsToSent(Pet pet) {
+    public void addPetPositiveTestShouldReturn200AndBodyEqualsToSent() {
+        log.info("Start test with pet: {}", pet);
         Response response = PetRequest.addNewPet(pet);
         CommonChecks.checkResponseCode(HttpStatus.SC_OK, response);
         PetChecks.checkPetBody(pet, response);
-        System.out.println(PetRequest.deletePet(pet.getId()).asString());
     }
+
+    @AfterEach
+    public void deleteTestData() {
+        PetRequest.deletePet(pet.getId());
+        log.info("End of test");
+    }
+
 }
